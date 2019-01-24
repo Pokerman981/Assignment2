df59d923ebace35ad26ea41ba97fbd8867a3c103
s0511527 <troy.gidney@lethbridgecollege.ca>
2019-01-24 09:19:27 -0700

    Initial commit

---
+++ src/me/troyg/assignment2/Hourly.java
@@ -0,0 +1,17 @@
+package me.troyg.assignment2;
+
+public class Hourly implements Payroll {
+
+    Employee employee = new Employee("0001", "TroyG");
+    
+
+    @Override
+    public void setPayPeriodDetails() {
+
+    }
+
+    @Override
+    public double calculatePay() {
+        return 0;
+    }
+}
---
+++ src/me/troyg/assignment2/Payroll.java
@@ -0,0 +1,6 @@
+package me.troyg.assignment2;
+
+public interface Payroll {
+     void setPayPeriodDetails();
+     double calculatePay();
+}
